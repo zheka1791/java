4d10478241650452b09e1b8ad8459eebe25a451a
zheka1791 <yevhen.dziuba@gmail.com>
2023-02-07 13:55:16 +0200

    First commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int myMinByteValue = 1132412423;
+        byte myTotal = (byte)(myMinByteValue / 2);
+        System.out.println(myTotal);
+    }
+}
